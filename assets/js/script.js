5a5455070080ee3bea7932878fd1ef14f1ffacc1
SamuelLauro <samuellauro0612@gmail.com>
2023-03-29 23:35:13 -0300

    finalizando o projeto

---
+++ assets/js/script.js
@@ -0,0 +1,25 @@
+const horas = document.querySelector('#horas')
+const minutos = document.querySelector('#minutos')
+const segundos = document.querySelector('#segundos')
+
+const relogio = setInterval(function time(){
+    let dateToday = new Date();
+    let hora = dateToday.getHours();
+    let min = dateToday.getMinutes();
+    let seg = dateToday.getSeconds();
+
+    if (hora < 10){
+        hora = '0' + hora
+    }
+    if (min < 10){
+        min = '0' + min
+    }
+    if (seg < 10){
+        seg = '0' + seg
+    }
+    
+
+    horas.textContent = hora;
+    minutos.textContent = min;
+    segundos.textContent = seg
+})
